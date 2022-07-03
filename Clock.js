11ed443fc9f22195cbc82f9002e80969489e2cdd
showvike <showvike@gmail.com>
2022-07-03 16:16:53 +0600

    converted to react

---
+++ Clock.js
@@ -0,0 +1,32 @@
+function Clock() {
+    const [time, setTime] = React.useState(new Date());
+
+    React.useEffect(() => {
+        setInterval(() => {
+            setTime(new Date());            
+        }, 100);
+    });
+
+    const [hours, minutes, seconds] = [time.getHours(),
+    time.getMinutes(), time.getSeconds()];
+
+    function format(value) {
+        return value < 10 ? "0" + value : value;
+    }
+
+    return (
+        <div className="main">
+            <div className="badge">
+                CLOCKLY
+            </div>
+            <div className="clock">
+                <span>{format(hours)}</span> :
+                <span>{format(minutes)}</span> :
+                <span>{format(seconds)}</span>
+            </div>
+        </div>
+    );
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<Clock />);
